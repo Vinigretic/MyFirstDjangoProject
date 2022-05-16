22c0a9afd2f93cf3b9bb5b48c7cb9f18ef3472b6
Vinigretic <v.bakatina@gmail.com>
2022-05-16 18:29:15 +0300

    README file: - create file - update urls(comment)

--- myFirstDjangoProject/urls.py
+++ myFirstDjangoProject/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from firstapp import views
+from firstapp import views # импорт из приложения
 
 urlpatterns = [
     path('', views.index, name='home'),
