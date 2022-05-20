f4d0be06fc5549ef645de4115c90cef83dacddf1
Vinigretic <v.bakatina@gmail.com>
2022-05-20 18:13:52 +0300

    Update parameters - update urls added parameters - update views added parameters

--- firstapp/views.py
+++ firstapp/views.py
@@ -2,20 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse('<h2>Главная страница</h2>')
+# def index(request):
+#     return HttpResponse('<h2>Главная страница</h2>')
+#
+# def about(request):
+#     return HttpResponse('<h2>О сайте</h2>')
+#
+# def users(request):
+#     return HttpResponse('<h2>Пользователи</h2>')
+#
+# def contacts(request):
+#     return HttpResponse('<h2>Контакты</h2>')
+#
+# def login(request):
+#     return HttpResponse('<h2>Логин</h2>')
+#
+# def registred(request):
+#     return HttpResponse('<h2>Регистрация</h2>')
 
-def about(request):
-    return HttpResponse('<h2>О сайте</h2>')
+def products(request, product_id = 0):
+    # output = '<h2>product № {0}</h2>'.format(product_id)
+    output = f'<h2>product № {product_id}</h2>'
+    return HttpResponse(output)
 
-def users(request):
-    return HttpResponse('<h2>Пользователи</h2>')
+def users(request, id = 0, name = 'admin'):
+    # output = '<h2>User</h2> <h3>id:{0} name:{1}</h3>'.format(id, name)
+    output = f'<h2>User</h2> <h3>id:{id} name:{name}</h3>'
+    return HttpResponse(output)
 
-def contacts(request):
-    return HttpResponse('<h2>Контакты</h2>')
-
-def login(request):
-    return HttpResponse('<h2>Логин</h2>')
-
-def registred(request):
-    return HttpResponse('<h2>Регистрация</h2>')
+def contacts(request, id = 0, name = 'admin', phone_number = '098-96-40-698'):
+    output = f'<h1>Contacts page</h1> <h2>id:{id}</h2> <p>name:{name}, phone_number:{phone_number} </p>'
+    return HttpResponse(output)
--- myFirstDjangoProject/urls.py
+++ myFirstDjangoProject/urls.py
@@ -2,12 +2,22 @@ from django.contrib import admin
 from django.urls import path
 from firstapp import views # импорт из приложения
 
+# urlpatterns = [
+#     path('', views.index, name='home'),
+#     path('admin/', admin.site.urls),
+#     path('about/', views.about),
+#     path('users/', views.users),
+#     path('contacts/', views.contacts),
+#     path('login/', views.login),
+#     path('registred/', views.registred),
+# ]
+
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('admin/', admin.site.urls),
-    path('about/', views.about),
+    path('products/', views.products),
+    path('products/<int:product_id>/', views.products),
     path('users/', views.users),
+    path('users/<int:id>/<name>/', views.users),
     path('contacts/', views.contacts),
-    path('login/', views.login),
-    path('registred/', views.registred),
+    path('contacts/<int:id>/<name>/<phone_number>/', views.contacts),
+    path('admin/', admin.site.urls),
 ]
