d626d9ce97485821f2030aa4878f7452d0a4ede6
Vinigretic <v.bakatina@gmail.com>
2022-06-22 19:26:17 +0300

    Field Type in Forms - lesson fifteen

--- firstapp/forms.py
+++ firstapp/forms.py
@@ -3,4 +3,16 @@ from django import forms
 class UserForm(forms.Form):
     name = forms.CharField()
     age = forms.IntegerField()
+    is_married = forms.BooleanField()
+    null_is_married = forms.NullBooleanField()
+    address = forms.EmailField()
+    ip_address = forms.GenericIPAddressField()
+    link = forms.URLField()
+    file_select = forms.FileField()
+    file_select_image = forms.ImageField()
+    data = forms.DateField()
+    time = forms.TimeField()
+    data_time = forms.SplitDateTimeField()
+    numb = forms.FloatField()
+    list1 = forms.ChoiceField(choices=((1, 'Kharkiv'), (2, 'Kyiv'), (3, 'Lviv')))
 
