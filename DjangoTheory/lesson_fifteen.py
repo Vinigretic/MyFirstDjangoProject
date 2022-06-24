5f6e642b21b0c5c3f0780fe5da7f5abc4264d8cc
Vinigretic <v.bakatina@gmail.com>
2022-06-24 18:16:24 +0300

    Update lesson fifteen, index.html, forms.py - Widget

--- DjangoTheory/lesson_fifteen.py
+++ DjangoTheory/lesson_fifteen.py
@@ -1,4 +1,5 @@
 # Типы полей в формах
+# from django import forms сделать импорт
 # forms.BooleanField - создает поле check-box, возвращает значение bool, True если флаг отмечен и False если нет
 # forms.NullBooleanField - создает поле у которого есть три варианта - неизвестный, да, нет
 # forms.CharField - создает поле для текстового ввода
@@ -15,3 +16,64 @@
 # forms.FloatField - создает поле для ввода не целых чисел
 # forms.ChoiceField(choises=((),())) - генерирует словарь из елементов кортежей, каждый кортеж должен
 # содержать два элемента, первый елемент кортежа будет ключем(value в HTML), второй - значением
+
+# С помощью парамерта label можем давать названия для полей
+# name = forms.CharField(label='What is your name stranger' )
+
+# Виджеты django
+# с помощью параметра widget - можем менять поведение форм
+# PasswordInput - генерирует поле для ввода пароля <input type='password'>
+# HiddenInput - генерирует скрытое поле <input type='hidden'>
+# MultipleHiddenInput - генерирует набор скрытых полей <input type='hidden'>
+# TextInput - генерирует однострочное текстовое поле
+# TextArea - генерирует многострочное текствое поле <textarea></textarea>
+# RadioSelect - генерирует список переключателей(радиокнопок) <input type='radio'>
+# CheckboxSelectMultiple - генерирует список флажков(чекбоксов) <input type='checkbox'>
+# TimeInput - генерирует поле для ввода времени
+# SelectDateWidget - генерирует три поля select для выбора дня, месяца и года
+# FileInput - генерирует поле для выбора файла
+
+# class UserForm(forms.Form):
+#     name = forms.CharField(label='What is your name stranger', initial='name', help_text=' Input name here')
+# initial - устанавдивает значение по умолчанию
+# help_text - устанавливает подсказку рядом с полем ввода
+
+# Пример передачи widgeta
+# comment = forms.CharField(label='Комментарий', widget=forms.Textarea, initial='Please type your comment',
+#                           help_text='Input comment here')
+# field_order - для того что бы поменять порядок расположения форм на странице испоьзуем это свойство
+# создаем переменную обязательно с названием field_order, передаем в нее список, и в этом списке в качестве аргументов
+# передаем название полей, в нужном порядке отображения
+
+# Настройка вида формы
+# Используются три спец метода:
+# 1. as_table(): отображение ввиде таблицы
+# 2. as_ul(): отображение ввиде списка
+# 3. as_p: отображение в каждом отдельном параграфе
+
+# Пример описания в HTML
+#
+# <form method="POST">
+#         {% csrf_token %}
+#         <table>
+#         {{ form.as_table }}
+#         </table>
+#         <input type="submit" value="send">
+# </form>
+
+# <form method="POST">
+#         {% csrf_token %}
+#         <ul>
+#         {{ form.as_ul }}
+#         </ul>
+#         <input type="submit" value="send">
+# </form>
+
+# <form method="POST">
+#         {% csrf_token %}
+#         <div>
+#         {{ form.as_p }}
+#         </div>
+#         <input type="submit" value="send">
+# </form>
+
--- firstapp/forms.py
+++ firstapp/forms.py
@@ -1,18 +1,27 @@
 from django import forms
 
+# class UserForm(forms.Form):
+#     name = forms.CharField(label='What is your name stranger' )
+#     age = forms.IntegerField()
+#     is_married = forms.BooleanField()
+#     null_is_married = forms.NullBooleanField()
+#     address = forms.EmailField()
+#     ip_address = forms.GenericIPAddressField()
+#     link = forms.URLField()
+#     file_select = forms.FileField()
+#     file_select_image = forms.ImageField()
+#     data = forms.DateField()
+#     time = forms.TimeField()
+#     data_time = forms.SplitDateTimeField()
+#     numb = forms.FloatField()
+#     list1 = forms.ChoiceField(choices=((1, 'Kharkiv'), (2, 'Kyiv'), (3, 'Lviv')))
+
 class UserForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
-    is_married = forms.BooleanField()
-    null_is_married = forms.NullBooleanField()
-    address = forms.EmailField()
-    ip_address = forms.GenericIPAddressField()
-    link = forms.URLField()
-    file_select = forms.FileField()
-    file_select_image = forms.ImageField()
-    data = forms.DateField()
-    time = forms.TimeField()
-    data_time = forms.SplitDateTimeField()
-    numb = forms.FloatField()
-    list1 = forms.ChoiceField(choices=((1, 'Kharkiv'), (2, 'Kyiv'), (3, 'Lviv')))
+    name = forms.CharField(label='What is your name stranger', initial='name', help_text=' Input name here')
+    age = forms.IntegerField(initial=18, help_text='Input age here')
+    comment = forms.CharField(label='Комментарий', widget=forms.Textarea, initial='Please type your comment',
+                              help_text='Input comment here')
+    field_order = ['age', 'name', 'comment']
+
+
 
